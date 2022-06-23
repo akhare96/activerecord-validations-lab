d2388d1fe441bd83d127677c448cc3f52020867f
Ambar Khare <ambar.khare24@gmail.com>
2022-06-23 12:20:48 -0700

    commit tree b397e3e86c3c27984d45c3fde8e29780bb66e321

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :check_clickbait
+
+
+    def check_clickbait
+        a = [/Won't Believe/i, /Secret/i, /Top [0-9]*/i, /Guess/i]
+        if a.none? {|b| b.match title }
+            errors.add(:title, "You need a clickbait title")
+        end
+    end
 end
